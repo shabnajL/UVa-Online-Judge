5c29eec14ffbf34dc60183ae51bff64b89631dec
Su_rose19 <shabnajliaqat@gmail.com>
2018-05-05 22:20:57 +0600

    Update UVA - 494(Kindergarten Counting Game).cpp

--- UVA - 494(Kindergarten Counting Game).cpp
+++ UVA - 494(Kindergarten Counting Game).cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long int ll;
-bool isalph(string s)
+/*bool isalph(string s)
 {
     bool f = false;
     for(int i=0; i<s.size(); i++)
@@ -12,20 +12,35 @@ bool isalph(string s)
         }
     }
     return f;
-}
+}        */
 int main()
 {
     string s,s1;
     while(getline(cin,s))
     {
+        int cnt = 0,f = 0;
+        /*
         istringstream ss(s);
-        int cnt = 0;
         while(ss>>s1)
         {
             if(isalph(s1)){
                 cnt = cnt + 1;
             }
+        }*/
+        for(int i=0;s[i];i++)
+        {
+            if((s[i]>='A' && s[i]<='Z') || (s[i]>='a' && s[i]<='z')){
+
+                f = 1;
+            }
+            else{
+                cnt = cnt + f;
+                f = 0;
+                
+
+            }
         }
+        cnt += f;
         cout<<cnt<<endl;
         s.clear();
         s1.clear();
